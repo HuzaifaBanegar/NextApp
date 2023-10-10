b057068d98a441e5a9611831bdaff5ed5a640c57
unknown <huzaifa@quickblink.io>
2023-10-10 16:07:22 +0530

    complete navbar mobile and pc version plus theme

--- my-app/components/shared/navbar/Navbar.tsx
+++ my-app/components/shared/navbar/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Theme from "./Theme";
+import MobileNav from "./Mobilenav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -18,7 +20,7 @@ const Navbar = () => {
           Dev<span className="text-primary-500">.Flow</span>
         </p>
       </Link>
-      GlobalSearch
+      <GlobalSearch/>
       <div className="flex-between gap-5">
         <Theme/>
         <SignedIn>
@@ -35,6 +37,7 @@ const Navbar = () => {
           ></UserButton>
         </SignedIn>
       </div>
+      <MobileNav/>
     </nav>
   );
 };
